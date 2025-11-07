7f010db59627f94ff03073661dad18aa334916f4
mirkobechini <mirkobechini@gmail.com>
2025-11-07 14:04:28 +0100

    Add: connection to database

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
+const connection = require("./database/connection")
+
 
 app.listen(PORT, ()=>{
     console.log(`Connected to http://localhost:${PORT}`);
