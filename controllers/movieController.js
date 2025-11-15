7924d1ff930825ba4e6d2c74931fe339302c7811
mirkobechini <mirkobechini@gmail.com>
2025-11-15 10:44:17 +0100

    Add: added movie created as response in creation query

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -32,30 +32,40 @@ function show(req, res) {
 
 function store(req, res) {
 
-    
+
 
     const { title, director, genre, release_year, abstract } = req.body
     const image = 'http://localhost:3000/uploads/' + req.file.originalname
 
-    
+
     const sql = "INSERT INTO movies (title, director, genre, release_year, abstract, image) VALUES (?, ?, ?, ?, ?, ?) "
 
     connection.query(sql, [title, director, genre, release_year, abstract, image], (err, result) => {
         if (err) return res.status(500).json({ error: err.message })
-        res.status(201).json({ message: "Movie added" })
+        res.status(201).json({
+            message: "Movie added",
+            movie: {
+                title,
+                director,
+                genre,
+                release_year,
+                abstract,
+                image
+            }
+        })
     })
 
 }
 
-function destroy(req, res){
+function destroy(req, res) {
     const movie_id = Number(req.params.id)
     const sql = "DELETE FROM movies WHERE id = ?"
-    
-    connection.query(sql, [movie_id], (err, result)=>{
-        if(err) return res.status(500).json({error: err.message})
-        res.status(204).json({message: `Movie with id ${movie_id} deleted`})
+
+    connection.query(sql, [movie_id], (err, result) => {
+        if (err) return res.status(500).json({ error: err.message })
+        res.status(204).json({ message: `Movie with id ${movie_id} deleted` })
     })
-    
+
 }
 
 function storeReviews(req, res) {
@@ -63,12 +73,12 @@ function storeReviews(req, res) {
     console.log(req.body);
 
     const movie_id = Number(req.params.id)
-    const { name, text, vote} = req.body
+    const { name, text, vote } = req.body
 
     const sql = "INSERT INTO reviews (movie_id, name, text, vote) VALUES (?, ?, ?, ?) "
 
 
-    connection.query(sql, [movie_id,name, text, vote], (err, result) => {
+    connection.query(sql, [movie_id, name, text, vote], (err, result) => {
         if (err) return res.status(500).json({ error: err.message })
         res.status(201).json({ message: "Review added" })
     })
